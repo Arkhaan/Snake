b0416ebabe25539d0f1c066eef8f96c8dd1eb5cc
Arkhaan <martin.binet29@gmail.com>
2021-01-07 15:31:34 +0100

    Draw field

---
+++ Snake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 20;
+            int height = 20;
+            Console.WriteLine("Ssssssnake");
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (x == 0 || x == width - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else if (y == 0 || y == height - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
